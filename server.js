d12db195d4373473bf3545237150e00f7f0795ce
Rafael <105018495+rafaelrlc@users.noreply.github.com>
2023-02-16 19:32:40 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger, logEvents } = require("./middleware/logEvents");
+const { send } = require("process");
+const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+app.use(express.urlencoded({ extended: false })); // built-in middleware to handle urlencoded data
+
+app.use(express.json()); // built-in middleware for json
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("^/$|index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page"); // 302 by default
+});
+
+// Route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello world!");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+
+const three = (req, res, next) => {
+  console.log("three");
+  res.send("Finished");
+};
+
+app.get("/chain", [one, two, three]);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  //res.redirect("/");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
