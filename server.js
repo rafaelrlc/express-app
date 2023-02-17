68f4028992b8dfe26cb2423f4565fdf650ad5c3a
Rafael <105018495+rafaelrlc@users.noreply.github.com>
2023-02-17 15:10:38 -0300

    update

--- server.js
+++ server.js
@@ -3,20 +3,16 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const { logger, logEvents } = require("./middleware/logEvents");
-const { send } = require("process");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
-const whitelist = [
-  "https://www.google.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
+const whitelist = ["http://127.0.0.1:5500", "http://localhost:3500"];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("NOT ALLOWED BY CORS"));
@@ -25,58 +21,33 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
+// Middleware
+
 app.use(cors(corsOptions)); // Cross Origin Resource Sharing
 
 app.use(express.urlencoded({ extended: false })); // built-in middleware to handle urlencoded data
-
 app.use(express.json()); // built-in middleware for json
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public"))); // set public
+app.use("/subdir", express.static(path.join(__dirname, "/public"))); // set public
 
-app.get("^/$|index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+app.use(errorHandler);
 
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
+// Routes
 
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page"); // 302 by default
-});
+app.use("/", require("./routes/root")); // set root("/") route
+app.use("/subdir", require("./routes/subdir")); // set subdir route
+app.use("/employees", require("./routes/api/employees"));
 
-// Route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello world!");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
   }
-);
-
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res, next) => {
-  console.log("three");
-  res.send("Finished");
-};
-
-app.get("/chain", [one, two, three]);
-
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  //res.redirect("/");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
