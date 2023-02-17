e3eb451ea6b3a30106be22504d93cc9fd76bdaa8
Rafael <105018495+rafaelrlc@users.noreply.github.com>
2023-02-16 23:37:35 -0300

    update

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 const { logger, logEvents } = require("./middleware/logEvents");
 const { send } = require("process");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+const whitelist = [
+  "https://www.google.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("NOT ALLOWED BY CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Cross Origin Resource Sharing
 
 app.use(express.urlencoded({ extended: false })); // built-in middleware to handle urlencoded data
 
