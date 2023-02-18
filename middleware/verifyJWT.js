17e5a15c139826780d5ca0fe59bbb6480a5f75d5
Rafael <105018495+rafaelrlc@users.noreply.github.com>
2023-02-18 20:55:38 -0300

    update

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401);
-  console.log(authHeader); // Baerer token
+  if (!authHeader) {
+    res.status(401);
+    return res.json({ 401: "Invalid Token" });
+  }
+  console.log(authHeader); // Bearer token
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); // invalid token (maybe expired)
+    if (err) return res.sendStatus(403); //invalid token
     req.user = decoded.username;
     next();
   });
--- routes/api/register.js
+++ routes/api/register.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { handleNewUser } = require("../../controllers/userRegisterController");
+const { handleNewUser } = require("../../controllers/registerController");
 
 router.post("/", handleNewUser);
 
