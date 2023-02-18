9cea8a87675b8f61f983466bf244f97ade0fbbd6
Rafael <105018495+rafaelrlc@users.noreply.github.com>
2023-02-18 20:55:38 -0300

    update

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const { logger, logEvents } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
@@ -16,16 +19,21 @@ app.use(cors(corsOptions)); // Cross Origin Resource Sharing
 app.use(express.urlencoded({ extended: false })); // built-in middleware to handle urlencoded data
 app.use(express.json()); // built-in middleware for json
 
-app.use("/", express.static(path.join(__dirname, "/public"))); // set public
+// Middleware for cookies
+app.use(cookieParser());
 
-app.use(errorHandler); // handle errors
+app.use("/", express.static(path.join(__dirname, "/public"))); // set public
 
 // Routes
 
 app.use("/", require("./routes/root")); // set root("/") route
-app.use("/employees", require("./routes/api/employees"));
 app.use("/register", require("./routes/api/register"));
 app.use("/auth", require("./routes/api/auth"));
+
+app.use(verifyJWT); // tudo abaixo vai precisar passar pelo verifyJWT middleware
+
+app.use("/employees", require("./routes/api/employees"));
+
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -36,5 +44,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 not found");
   }
 });
+
+app.use(errorHandler); // handle errors
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
