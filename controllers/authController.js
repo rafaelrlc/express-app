b8daa3d515b9a71d18b66090683b0adcbc603073
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-02-20 13:32:57 -0300

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,6 +54,8 @@ const userLogin = async (req, res) => {
 
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
+      //sameSite: "None",
+      //secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({ accessToken });
