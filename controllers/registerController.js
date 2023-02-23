e3900d06380e91f860893c2786474cd2b92df81b
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-02-22 22:30:20 -0300

    update

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,14 +1,5 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
-const fsPromises = require("fs").promises;
-const path = require("path");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
-const { json } = require("express");
 
 const handleNewUser = async (req, res) => {
   const { user, pwd } = req.body;
@@ -18,7 +9,7 @@ const handleNewUser = async (req, res) => {
       .json({ message: "Username and password are required" });
   }
   // check for duplicate useraname in db
-  const duplicate = usersDB.users.find((person) => person.username === user);
+  const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate) {
     return res.sendStatus(409); // conflict
   }
@@ -26,19 +17,12 @@ const handleNewUser = async (req, res) => {
     const hashedPwd = await bcrypt.hash(pwd, 10);
 
     // store the new user
-    const newUser = {
+    const result = await User.create({
       username: user,
-      roles: {
-        User: 2001,
-      },
       password: hashedPwd,
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    //console.log(usersDB.users);
+    });
+
+    console.log(result);
     res.status(201).json({ success: `New user ${user} created!` });
   } catch (error) {
     res.status(500).json({ message: error.message });
